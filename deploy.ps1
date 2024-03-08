9e2cc5c3539199a7aa50515028532db633e24fa6
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2024-03-07 20:49:35 -0700

    test

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,2 @@
 $ErrorActionPreference = "Stop"
-Connect-AzAccount
-Set-AzContext -SubscriptionName 'Enter your subscription name here'
 New-AzResourceGroup -Name 'TestRG' -Location 'Central US' -Tag @{'RG'='APP'}
