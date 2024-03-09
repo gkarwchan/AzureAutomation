58db48cbad6b27e9393a0a0d106c16c2cd4553ce
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2024-03-09 07:00:53 -0700

    wip

--- StorageAccounts/deploy.ps1
+++
@@ -1,1 +0,0 @@
-New-AzResourceGroup -Name automation -Location EastUS
--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 $ErrorActionPreference = "Stop"
-New-AzResourceGroup -Name 'TestRG' -Location 'Central US' -Tag @{'RG'='APP'}
+New-AzResourceGroup -Name 'TestRG' -Location 'Central US' -Force -Tag @{'RG'='APP'}
 New-AzResourceGroupDeployment -Name 'DeployTest' -ResourceGroupName 'TestRG' -TemplateFile './main.bicep'
