f0f6cb13e237189bd01cdbcaef0ac0566b91540c
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2024-03-11 21:46:34 -0600

    add service principle

--- deploy-rg-keyvault.ps1
+++ deploy-rg-keyvault.ps1
@@ -6,6 +6,21 @@ $secretvalue = ConvertTo-SecureString "....." -AsPlainText -Force
 Set-AzKeyVaultSecret -VaultName "dev-keyvault-gkar" -Name "sqlPassword" -SecretValue $secretvalue
 New-AzResourceGroup -Name 'DevGroup' -Location 'CentralUS' -Force -Tag @{'RG'='APP'}
 
+$servicePrincipal = New-AzADServicePrincipal -DisplayName gkar-pipeline-dev
+$serviceSecret = $servicePrincipal.PasswordCredentials.SecretText
+
+Write-Output "Service Principal application ID: $($servicePrincipal.AppId)"
+Write-Output "Service Principal ID: $($serviceSecret)"
+Write-Output "Azure Tenant ID: $((Get-AzContext).Tenant.Id)"
+
+New-AzRoleAssignment `
+    -ApplicationId $servicePrincipal.AppId `
+    -RoleDefinitionName Contributor `
+    -Scope '/subscriptions/f0750bbe-ea75-4ae5-b24d-a92ca601da2c/resourceGroups/DevGroup' `
+    -Description "The deployment pipeline for the company's website needs to be able to create resources within the resource group."
+
+    $servicePrincipal = New-AzADServicePrincipal -DisplayName MyPipeline -Role Contributor -Scope '/subscriptions/f0750bbe-ea75-4ae5-b24d-a92ca601da2c/resourceGroups/ToyWebsite'
+    
 # Get-AzKeyVaultSecret -VaultName "dev-keyvault-gkar" -Name "sqlPassword" -AsPlainText
 
 # $keyVaultName = 'YOUR-KEY-VAULT-NAME'
--- deploy-solution.ps1
+++ deploy-solution.ps1
@@ -1,2 +1,2 @@
 $rndm = Get-Random -Minimum -100 -Maximum 100
-New-AzResourceGroupDeployment -Name "Deploy-gkar-solution" -ResourceGroupName 'DevGroup' -TemplateFile './main.bicep' -TemplateParameterFile  dev.param.json
+New-AzResourceGroupDeployment -Name "Deploy-gkar-solution" -ResourceGroupName 'DevGroup' -TemplateFile infra/main.bicep -TemplateParameterFile  infra/dev.param.json
